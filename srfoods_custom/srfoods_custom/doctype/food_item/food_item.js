1dc41fefb896c38c37ad51b22c8167bccd84dba7
suvankarbbm143-web <suvankarbbm143@gmail.com>
2026-08-22 19:40:59 +0000

    added food item doctype

---
+++ srfoods_custom/srfoods_custom/doctype/food_item/food_item.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, suvankar and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Food Item", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ srfoods_custom/srfoods_custom/doctype/food_item/test_food_item.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, suvankar and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestFoodItem(FrappeTestCase):
+	pass
